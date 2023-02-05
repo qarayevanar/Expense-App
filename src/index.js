fdfda5be98304e0333adc60a4245976ef45644f5
Anar Qarayev <qarayev90anar90@gmail.com>
2023-02-05 18:43:16 +0400

    first app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
